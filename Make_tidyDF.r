e56529d54d2afc26344511a0f43f8a4ac741a149
James Broadbent <bro915@pearcey-login.cm.hpc>
2019-05-03 11:13:44 +1000

    script for making a tidy DF from file names and metadata

---
+++ Make_tidyDF.r
@@ -0,0 +1,18 @@
+#set the working directory
+setwd("/OSM/CBR/AF_DATASCHOOL/output/bro915/")
+#load tidyverse
+library("tidyverse")
+#load objects and fix					
+samples <- read_csv("FilesDF.csv")					
+
+list_file <- list.files(full.names=T, pattern = "*.out")%>%
+  lapply(read_csv) %>%
+  bind_rows
+
+files <- read_csv("FilesDF.csv")
+
+files$filenames <- gsub(".gz", "", files$filenames)
+
+tidyDF <- merge(files, list_file, by=c("filename"))
+
+write_csv(tidyDF, path = "/OSM/CBR/AF_DATASCHOOL/output/bro915/TidyDF.csv")
